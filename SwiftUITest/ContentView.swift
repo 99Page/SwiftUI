6ecc082b29b0d6e56218e89f2b9938c653a28517
99Page <nou0ggid@gmail.com>
2022-05-29 23:11:00 +0900

    king fisher

--- SwiftUITest/ContentView.swift
+++ SwiftUITest/ContentView.swift
@@ -6,11 +6,21 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct ContentView: View {
     
     var body: some View {
-        ImageViewTester()
+        VStack {
+            Text("KFImage")
+            KFImage(URL(string: "https://i.postimg.cc/mZ52x6h8/Unknown.png")!)
+                .onFailure({ error in
+                    print("Error : \(error)")
+                })
+                .resizable()
+                .frame(width: 100, height: 100)
+                .clipped()
+        }
     }
 }
 
